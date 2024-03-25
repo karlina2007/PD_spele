455803912ca7f8d0ee3cd16f292fbb8d3e839294
karlina2007 <152556927+karlina2007@users.noreply.github.com>
2024-03-25 10:06:18 +0200

    Add files via upload

---
+++ PD_karlina_korsuna.py
@@ -0,0 +1,25 @@
+from random import randint
+
+# atslegas = list(valstis.keys())
+
+
+while True:
+    g = input("Kā varu palīdzēt?")
+    if g.lower() == "q":
+        print("Paldies par spēli!")
+        break
+    elif g.lower() == "ģ":
+        print('kartiņa')
+       
+    elif g.lower() =="bingo":
+        print("Malacis!")
+        break
+    elif g.lower() =="s":
+        print("skaitlis")
+    else:
+        print("Nesapratu")
+
+
+
+
+
